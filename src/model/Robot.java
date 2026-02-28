084936bfec2e39153fa668fa515d882c0c94601f
joker10130 <narongrit.rodphroboon@outlook.co.th>
2026-02-28 13:15:31 +0700

    Complete toy robot challenge

---
+++ src/model/Robot.java
@@ -0,0 +1,18 @@
+package model;
+
+public class Robot {
+    private int x;
+    private int y;
+    private Direction direction;
+//    Check if place has been add first
+    private boolean isPlaced = false;
+
+    public int getX() { return x; }
+    public void setX(int x) { this.x = x; }
+    public int getY() { return y; }
+    public void setY(int y) { this.y = y; }
+    public Direction getDirection() { return direction; }
+    public void setDirection(Direction direction) { this.direction = direction; }
+    public boolean isPlaced() { return isPlaced; }
+    public void setPlaced(boolean placed) { isPlaced = placed; }
+}
